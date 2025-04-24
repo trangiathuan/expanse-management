22e585b0c8b8c5151dac4108371046e41774191e
trangiathuan <nakrothfaker@krothfakergmail.fakergmailcom>
2025-04-24 17:09:07 +0700

    .

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -151,24 +151,31 @@ const Home = () => {
                                 </div>
 
                             </div>
-                            {filterExpenses.map((item, index) => {
-                                const date = new Date(item.date);
-                                const formattedDate = isNaN(date.getTime()) ? 'Invalid date' : date.toLocaleDateString('vi-VN');
-                                return (
-                                    <div key={index} className="shadow-lg rounded-lg mt-2 bg-white">
-                                        <div className="cursor-pointer flex justify-between">
-                                            <p className="mt-1 ms-2 text-sm font-semibold text-gray-500">{formattedDate}</p>
-                                            <p className="mt-1 me-2 text-sm font-semibold text-gray-500 flex items-center"><Eye className="w-4 h-4 me-1" />Chi tiết</p>
+                            {expenses ? (
+                                filterExpenses.map((item, index) => {
+                                    const date = new Date(item.date);
+                                    const formattedDate = isNaN(date.getTime()) ? 'Invalid date' : date.toLocaleDateString('vi-VN');
+                                    return (
+                                        <div key={index} className="shadow-lg rounded-lg mt-2 bg-white">
+                                            <div className="cursor-pointer flex justify-between">
+                                                <p className="mt-1 ms-2 text-sm font-semibold text-gray-500">{formattedDate}</p>
+                                                <p className="mt-1 me-2 text-sm font-semibold text-gray-500 flex items-center"><Eye className="w-4 h-4 me-1" />Chi tiết</p>
+                                            </div>
+                                            <div className="flex space-x-1 p-1 text-sm pb-2">
+                                                <p className={`${item.type === 'Chi tiêu' ? 'bg-red-200 text-red-600 rounded-lg' : 'bg-green-200 text-green-600 rounded-lg'} font-semibold w-24 h-7 p-1 flex justify-center items-center`}>{item.type}</p>
+                                                <p className={`bg-gray-100 ${item.type === 'Chi tiêu' ? 'text-red-600' : 'text-green-600'}  rounded-lg font-semibold w-28 h-7 p-1 flex justify-center`}>{item.type === 'Chi tiêu' ? '-' : '+'} {item.amount.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })}</p>
+                                                <p className="bg-yellow-200 text-yellow-600 rounded-lg font-semibold w-20 h-7 p-1 flex justify-center">{item.category}</p>
+                                                <p className="bg-blue-200 text-blue-600 rounded-lg font-semibold w-28 h-7 p-1 flex justify-center">{item.total.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })}</p>
+                                            </div>
                                         </div>
-                                        <div className="flex space-x-1 p-1 text-sm pb-2">
-                                            <p className={`${item.type === 'Chi tiêu' ? 'bg-red-200 text-red-600 rounded-lg' : 'bg-green-200 text-green-600 rounded-lg'} font-semibold w-24 h-7 p-1 flex justify-center items-center`}>{item.type}</p>
-                                            <p className={`bg-gray-100 ${item.type === 'Chi tiêu' ? 'text-red-600' : 'text-green-600'}  rounded-lg font-semibold w-28 h-7 p-1 flex justify-center`}>{item.type === 'Chi tiêu' ? '-' : '+'} {item.amount.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })}</p>
-                                            <p className="bg-yellow-200 text-yellow-600 rounded-lg font-semibold w-20 h-7 p-1 flex justify-center">{item.category}</p>
-                                            <p className="bg-blue-200 text-blue-600 rounded-lg font-semibold w-28 h-7 p-1 flex justify-center">{item.total.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })}</p>
-                                        </div>
-                                    </div>
-                                );
-                            })}
+                                    );
+                                })
+                            ) :
+                                (
+                                    <div>Loading...</div>
+                                )
+                            }
+
                         </div>
                     )}
 
